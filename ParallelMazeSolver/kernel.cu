ee41469afcfe4d37b7468e6b79ec7d0a584f33f2
Romain Couperier <romain.couperier@mail.mcgill.ca>
2019-11-26 18:36:19 -0500

    Pictured Path

--- ParallelMazeSolver/kernel.cu
+++ ParallelMazeSolver/kernel.cu
@@ -19,7 +19,6 @@ __global__ void checkPoint(Point* d_points, Point* d_new_points, unsigned char* 
 	// shared array to hold resulting new points to visit for next iteration
 	__shared__ Point new_points[2 * (N < M ? N : M)];
 
-	//for (int index = (blockIdx.x * nb_threads + threadIdx.x); index < N; index += nb_threads) {
 	for (int index = (blockIdx.x * nb_threads + threadIdx.x); index < N; index += nb_threads) {
 
 		new_points[index] = Point(-1, -1);
@@ -33,9 +32,7 @@ __global__ void checkPoint(Point* d_points, Point* d_new_points, unsigned char* 
 			int col = d_points[index].getC();
 
 			// If point above is in bounds and not a wall
-			//if (row - 1 >= 0 && maze[(row - 1) * N + col]) {
 			if (row - 1 >= 0 && image[(row - 1) * 4 * N + col * 4 + 2] > 250) {
-				//printf("Point above: x: %d, y: %d, value: %d\n", (row-1), col, maze[(row - 1) * N + col]);
 				// Insert in shared array and get insertion index
 				if (insertPoint(new_points, Point((row - 1), col))) {
 					new_points[atomicAdd(&insert_index[0], 1)] = Point((row - 1), col);
@@ -43,22 +40,11 @@ __global__ void checkPoint(Point* d_points, Point* d_new_points, unsigned char* 
 					image[(row - 1) * 4 * N + col * 4 + 1] = 0;
 					image[(row - 1) * 4 * N + col * 4 + 2] = 0;
 				}
-				// If successfully inserted, print details
-				//printf("row: %d, col: %d, i: %d\n", (row - 1), col, i);
-				/*if (i >= 0) {
-					printf("Point %d, r: %d, c: %d\n", i, new_points[i].getR(), new_points[i].getC());
-				} */
 			}
 
-			// Synchronize threads before checkpoint points to the left
-			//__syncthreads();
-
-			//printf("Entering Left Point if statement\n");
-
+			
 			// If point to the left is in bounds and not a wall
-			//if (col - 1 >= 0 && maze[row * N + (col - 1)]) {
 			if (col - 1 >= 0 && image[row * 4 * N + (col - 1) * 4 + 2] > 250) {
-				//printf("Point to the left: x: %d, y: %d, value: %d\n", row, (col - 1), maze[row * N + (col - 1)]);
 				// Insert in shared array and get insertion index
 				if (insertPoint(new_points, Point(row, (col - 1)))) {
 					new_points[atomicAdd(&insert_index[0], 1)] = Point(row, (col - 1));
@@ -66,24 +52,10 @@ __global__ void checkPoint(Point* d_points, Point* d_new_points, unsigned char* 
 					image[row * 4 * N + (col - 1) * 4 + 1] = 0;
 					image[row * 4 * N + (col - 1) * 4 + 2] = 0;
 				}
-				/*int i = insertPoint(new_points, Point(row, (col - 1)));
-				printf("row: %d, col: %d, i: %d\n", row, (col - 1), i);
-				if (i >= 0) {
-					printf("Point %d, r: %d, c: %d\n", i, new_points[i].getR(), new_points[i].getC());
-					image[row * 4 * N + (col - 1) * 4] = 255;
-					image[row * 4 * N + (col - 1) * 4 + 1] = 0;
-					image[row * 4 * N + (col - 1) * 4 + 2] = 0;
-					// If successfully inserted, print details
-					//printf("Point %d, r: %d, c: %d\n", i, row, (col-1));
-				}*/
 			}
 
-			//__syncthreads();
-
 			// If point below is in bounds and not a wall
-			//if (row + 1 >= 0 && maze[(row - 1) * N + col]) {
 			if (row + 1 < M  && image[(row + 1) * 4 * N + col * 4 + 2] > 250) {
-				//printf("Point above: x: %d, y: %d, value: %d\n", (row-1), col, maze[(row - 1) * N + col]);
 				// Insert in shared array and get insertion index
 				if (insertPoint(new_points, Point((row + 1), col))) {
 					new_points[atomicAdd(&insert_index[0], 1)] = Point((row + 1), col);
@@ -91,25 +63,10 @@ __global__ void checkPoint(Point* d_points, Point* d_new_points, unsigned char* 
 					image[(row + 1) * 4 * N + col * 4 + 1] = 0;
 					image[(row + 1) * 4 * N + col * 4 + 2] = 0;
 				}
-				/*int i = insertPoint(new_points, Point((row + 1), col));
-				// If successfully inserted, print details
-				printf("row: %d, col: %d, i: %d\n", (row + 1), col, i);
-				if (i >= 0) {
-					printf("Point %d, r: %d, c: %d\n", i, new_points[i].getR(), new_points[i].getC());
-					image[(row + 1) * 4 * N + col * 4] = 255;
-					image[(row + 1) * 4 * N + col * 4 + 1] = 0;
-					image[(row + 1) * 4 * N + col * 4 + 2] = 0;
-					//printf("Point %d, r: %d, c: %d\n", i, (row - 1), col);
-				} */
 			}
 
-			//printf("Entering Left Point if statement\n");
-			//__syncthreads();
-
 			// If point to the left is in bounds and not a wall
-			//if (col - 1 >= 0 && maze[row * N + (col - 1)]) {
 			if (col + 1 < N && image[row * 4 * N + (col + 1) * 4 + 2] > 250) {
-				//printf("Point to the left: x: %d, y: %d, value: %d\n", row, (col - 1), maze[row * N + (col - 1)]);
 				// Insert in shared array and get insertion index
 				if (insertPoint(new_points, Point(row, (col + 1)))) {
 					new_points[atomicAdd(&insert_index[0], 1)] = Point(row, (col + 1));
@@ -117,16 +74,6 @@ __global__ void checkPoint(Point* d_points, Point* d_new_points, unsigned char* 
 					image[row * 4 * N + (col + 1) * 4 + 1] = 0;
 					image[row * 4 * N + (col + 1) * 4 + 2] = 0;
 				}
-				/* int i = insertPoint(new_points, Point(row, (col + 1)));
-				printf("row: %d, col: %d, i: %d\n", row, (col + 1), i);
-				if (i >= 0) {
-					printf("Point %d, r: %d, c: %d\n", i, new_points[i].getR(), new_points[i].getC());
-					image[row * 4 * N + (col + 1) * 4] = 255;
-					image[row * 4 * N + (col + 1) * 4 + 1] = 0;
-					image[row * 4 * N + (col + 1) * 4 + 2] = 0;
-					// If successfully inserted, print details
-					//printf("Point %d, r: %d, c: %d\n", i, row, (col-1));
-				} */
 			}
 		}
 
@@ -134,37 +81,21 @@ __global__ void checkPoint(Point* d_points, Point* d_new_points, unsigned char* 
 		__syncthreads();
 		d_new_points[index] = new_points[index];
 		d_new_points[index + (N < M ? N : M)] = new_points[index + (N < M ? N : M)];
-		//printf("index: %d, row: %d\n", index, d_new_points[index].getR());
-		//printf("index: %d, point: x:%d, y:%d\n", index, new_points[index].getR(), new_points[index].getC());
 	}
 }
 
-// insert given point at the first available position in the given array (avoiding duplicate points)
+// Insert given point at the first available position in the given array (avoiding duplicate points)
 __device__ bool insertPoint(Point array[2 * (N < M ? N : M)], Point point) {
 	int i;
 	// Cycle through array points until the end or an empty point (ie, still at initial Point(-1,-1) value) is reached
 	for (i = 0; i < 2 * (N < M ? N : M) && array[i].getR() >= 0; i++) {
-		// if duplicate point found (ie, point we want to insert is already in the array) do nothing and return
+		// If duplicate point found (ie, point we want to insert is already in the array) do nothing and return
 		if (point.getR() == array[i].getR() && point.getC() == array[i].getC()) {
-			//printf("point already exists\n");
 			return false;
 		}
 	}
 	if (i < 2 * (N < M ? N : M)) return true;
-	/*// If empty point has been found (and we are not at the end of the array), insert point and return insertion index
-	if (i < 2 * (N < M ? N : M)) {
-		array[i] = point;
-		return i;
-	}
-	// If array end is reached, do nothing and return
-	//printf("end of array reached\n");*/
 	return false;
-}
-
-__global__  void testAdd(int* a)
-{
-	int b = atomicAdd(&a[0], 1);
-	printf("%d\n", b);
 }
 
 int main(int argc, char* argv[])
@@ -200,14 +131,6 @@ int main(int argc, char* argv[])
 	error = lodepng_decode32_file(&image, &image_width, &image_height, input_filename);
 	if (error) printf("error %u: %s\n", error, lodepng_error_text(error)); 
 
-	/* // The maze, false is wall, true is path
-	bool maze[M * N] = {true, true, true, true,
-						true, true, false, true,
-						true, true, false, true,
-						true, false, true, true};
-
-	printf("Maze size: %d rows by %d columns\n", M, N); */
-
 	printf("Input file: %s, maze width: %d, maze height: %d\n", input_filename, image_width, image_height);
 	printf("Number of blocks: %d, number of threads: %d\n", nb_blocks, nb_threads);
 
@@ -218,14 +141,6 @@ int main(int argc, char* argv[])
 		Point points[diagonalSize];
 		// Set first point to be visited - the arrival point (because we are backtracking), the last point of the maze (assuming square maze)
 		points[0] = Point(M - 1, N - 1);
-		
-		/* Point* failedPoints;
-		cudaMallocManaged((void**)& failedPoints, N * N * sizeof(Point)); */
-
-		/*// Cuda copy of the maze
-		bool* d_maze;
-		cudaMallocManaged((void**)& d_maze, M * N * sizeof(bool));
-		cudaMemcpy(d_maze, &maze, M * N * sizeof(bool), cudaMemcpyHostToDevice);*/
 
 		cudaMallocManaged((void**)& image_copy, image_width * image_height * 4 * sizeof(unsigned char));
 		cudaMemcpy(image_copy, image, image_width * image_height * 4 * sizeof(unsigned char), cudaMemcpyHostToDevice);
@@ -273,7 +188,7 @@ int main(int argc, char* argv[])
 			cudaMemcpy(&points, d_new_points, diagonalSize * sizeof(Point), cudaMemcpyDeviceToHost);
 
 			run_num++;
-			if (run_num % 150 == 0) run_num = 0;
+			if (run_num % 10 == 0) run_num = 0;
 		}
 
 		lodepng_encode32_file("solvedMaze.png", image_copy, image_width, image_height);
@@ -291,21 +206,130 @@ int main(int argc, char* argv[])
 	ftime(&end_time);
 	total_time = 1000 * (end_time.time - start_time.time) + (end_time.millitm - start_time.millitm);
 
-	printf("Total execution time: %d, Parallel execution time: %d", (int)total_time, (int)cuda_total);
+	printf("Total execution time: %d, Parallel execution time: %d\n", (int)total_time, (int)cuda_total);
 
+
+	struct timeb s_time, e_time;
+	ftime(&s_time);
 	postProcessing("solvedMaze.png");
+	ftime(&e_time);
+	float post_time = 1000 * (e_time.time - s_time.time) + (e_time.millitm - s_time.millitm);
+
+	printf("Post processing time: %d", (int)post_time);
 
 	return 0;
 }
 
+int findNextIndex(unsigned char* image, int index, int indexValue, int h, int w) {
+	int nextIndex = -1;
+
+	if (indexValue == 255) indexValue = 245;
+
+	int r = index / (w * 4);
+	int c = index % (w * 4);
+
+	unsigned current = r * 4 * w + c;
+	unsigned right = current + 4;
+	unsigned down = current + 4 * w;
+	unsigned left = current - 4;
+	unsigned up = current - 4 * w;
+
+	if (r == 0 && c == 0) {
+		if (image[right] == indexValue + 1) {
+			nextIndex = right;
+		}
+		if (image[down] == indexValue + 1) {
+			nextIndex = down;
+		}
+	}
+
+	else if (r == 0) {
+		if (image[right] == indexValue + 1) {
+			nextIndex = right;
+		}
+		if (image[down] == indexValue + 1) {
+			nextIndex = down;
+		}
+		if (image[left] == indexValue + 1) {
+			nextIndex = left;
+		}
+	}
+
+	else if (r == h - 1) {
+		if (image[right] == indexValue + 1) {
+			nextIndex = right;
+		}
+		if (image[up] == indexValue + 1) {
+			nextIndex = up;
+		}
+		if (image[left] == indexValue + 1) {
+			nextIndex = left;
+		}
+	}
+
+	else if (c == 0) {
+		if (image[up] == indexValue + 1) {
+			nextIndex = up;
+		}
+		if (image[down] == indexValue + 1) {
+			nextIndex = down;
+		}
+		if (image[right] == indexValue + 1) {
+			nextIndex = right;
+		}
+	}
+
+	else if (c == w - 1) {
+		if (image[up] == indexValue + 1) {
+			nextIndex = up;
+		}
+		if (image[down] == indexValue + 1) {
+			nextIndex = down;
+		}
+		if (image[left] == indexValue + 1) {
+			nextIndex = left;
+		}
+	}
+
+	else {
+		if (image[up] == indexValue + 1) {
+			nextIndex = up;
+		}
+		if (image[down] == indexValue + 1) {
+			nextIndex = down;
+		}
+		if (image[left] == indexValue + 1) {
+			nextIndex = left;
+		}
+		if (image[right] == indexValue + 1) {
+			nextIndex = right;
+		}
+	}
+
+	return nextIndex;
+}
+
 void postProcessing(char* inputImage) {
 	unsigned error;
-	unsigned char* image, * image_copy;
+	unsigned char* image;
 	unsigned image_width, image_height;
 
 	error = lodepng_decode32_file(&image, &image_width, &image_height, inputImage);
 	if (error) printf("error %u: %s\n", error, lodepng_error_text(error));
 
+	int index = 0;
 
+	while (index != image_width * image_height - 1) {
+		int indexValue = image[index];
+		image[index] = (char)0;
+		image[index + 1] = (char)255;
+
+		index = findNextIndex(image, index, indexValue, image_width, image_height);
+
+		if (index == -1) {
+			lodepng_encode32_file("solvedMaze.png", image, image_width, image_height);
+			return;
+		}
+	}
 }
 
